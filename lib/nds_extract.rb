4ce80a5cc01ed458c5e46d9cd40712364ed1409d
Daniel Loyd <dhloyd@icloud.com>
2020-02-02 19:59:12 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -6,11 +6,57 @@ require 'directors_database'
 # { directorOne => allTheMoneyTheyMade, ... }
 
 def directors_totals(nds)
-  result = {}
-  nil
+  director_array = Array.new
+  total_array = Array.new
+  new_aoh = {}
+  count = 0
+  directors = nds.to_a.size
+  
+#===============================
+  
+    nds.each { |n| 
+      director_array << n[:name].to_s}
+    
+  directors.times {
+    nds[count][:movies].each { |n| 
+        total_array << n[:worldwide_gross] 
+        new_aoh[director_array[count]] = total_array.sum.to_i}
+    total_array.clear
+    count += 1}
+  
+  p new_aoh
+  p nds[0]
 end
+
+directors_totals(directors_database)
+
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
-def gross_for_director(director_data)
-end
+
+# def gross_for_director(director_data)
+  
+#   director_array = Array.new
+#   total_array = Array.new
+#   new_aoh = {}
+#   count = 0
+#   directors = nds.to_a.size
+  
+# #===============================
+  
+#     director_data.each { |n| 
+#       director_array << n[:name].to_s}
+    
+#   directors.times {
+#     director_data[count][:movies].each { |n| 
+#         total_array << n[:worldwide_gross] 
+#         new_aoh[director_array[count]] = total_array.sum.to_i}
+#     total_array.clear
+#     count += 1}
+  
+#   p new_aoh
+# end
+  
+# gross_for_director(director_data)
+
+
