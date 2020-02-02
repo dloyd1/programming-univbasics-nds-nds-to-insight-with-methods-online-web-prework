7bbc41aedd8dd48d4f70bfd260b78a9c32926ca2
Daniel Loyd <dhloyd@icloud.com>
2020-02-02 20:02:13 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -27,7 +27,6 @@ def directors_totals(nds)
   p new_aoh
 end
 
-#directors_totals(directors_database)
 
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
@@ -35,27 +34,26 @@ end
 
 # def gross_for_director(director_data)
   
-#   director_array = Array.new
-#   total_array = Array.new
-#   new_aoh = {}
-#   count = 0
-#   directors = nds.to_a.size
+  director_array = Array.new
+  total_array = Array.new
+  new_aoh = {}
+  count = 0
+  directors = nds.to_a.size
   
-# #===============================
+#===============================
   
-#     director_data.each { |n| 
-#       director_array << n[:name].to_s}
+    director_data.each { |n| 
+      director_array << n[:name].to_s}
     
-#   directors.times {
-#     director_data[count][:movies].each { |n| 
-#         total_array << n[:worldwide_gross] 
-#         new_aoh[director_array[count]] = total_array.sum.to_i}
-#     total_array.clear
-#     count += 1}
+  directors.times {
+    director_data[count][:movies].each { |n| 
+        total_array << n[:worldwide_gross] 
+        new_aoh[director_array[count]] = total_array.sum.to_i}
+    total_array.clear
+    count += 1}
   
-#   p new_aoh
+  p new_aoh
 # end
   
-# gross_for_director(director_data)
 
 
